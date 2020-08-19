c522ee4870e06b5945dfa2eb69e6487aa842689c
Anton Kalyutich <anton.kalyutich@gmail.com>
2020-08-19 22:27:59 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -54,7 +54,6 @@ def current_player(board)
   turn_count(board).even? ? "X" : "O"
 end
 
-
 def won?(board)
   WIN_COMBINATIONS.detect do |combo|
     (board[combo[0]] == "X" && board[combo[1]] == "X" && board[combo[2]] == "X") || (board[combo[0]] == "O" && board[combo[1]] == "O" && board[combo[2]] == "O")
